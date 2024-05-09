02157a7fc903a0556d1209dfcfdfb0e8f3969a11
jorda <jorda@LAPTOP-4BQV6VKK>
2024-05-09 14:56:38 +0200

    Add toggle button at the bottom right of the main page

--- functionality/index.js
+++ functionality/index.js
@@ -1,11 +1,26 @@
-const signUpButton = document.getElementById('signUp');
-const signInButton = document.getElementById('signIn');
-const container = document.getElementById('container');
+var button = document.getElementById("theme-toggle-btn");
+var dropdown = document.getElementById("theme-dropdown");
 
-signUpButton.addEventListener('click', () => {
-	container.classList.add("right-panel-active");
+button.addEventListener("click", function() {
+  dropdown.classList.toggle("show");
 });
 
-signInButton.addEventListener('click', () => {
-	container.classList.remove("right-panel-active");
+window.addEventListener("click", function(event) {
+  if (!event.target.matches('.theme-toggle-btn')) {
+    if (dropdown.classList.contains('show')) {
+      dropdown.classList.remove('show');
+    }
+  }
+});
+
+document.getElementById("dark-theme").addEventListener("click", function() {
+  document.body.style.backgroundColor = "#333";
+  document.body.style.color = "#fff";
+  document.getElementById("theme-toggle-btn").style.backgroundColor = "#333";
+});
+
+document.getElementById("white-theme").addEventListener("click", function() {
+  document.body.style.backgroundColor = "#fff";
+  document.body.style.color = "#333";
+  document.getElementById("theme-toggle-btn").style.backgroundColor = "#fff";
 });
