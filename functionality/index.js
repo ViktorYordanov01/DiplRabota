325e7f2096b01d058d506c80a7e811850b7c1b4a
jorda <jorda@LAPTOP-4BQV6VKK>
2024-05-12 21:51:45 +0200

    Modify html files and add toggle button on all of them

--- functionality/index.js
+++ functionality/index.js
@@ -13,14 +13,42 @@ window.addEventListener("click", function(event) {
   }
 });
 
+function setThemePreference(theme) {
+  localStorage.setItem('themePreference', theme);
+}
+
+function getThemePreference() {
+  return localStorage.getItem('themePreference');
+}
+
+function applyTheme(theme) {
+  if (theme === 'dark') {
+    document.body.style.backgroundColor = "#333";
+    document.body.style.color = "#fff";
+    document.getElementById("theme-toggle-btn").style.backgroundColor = "#333"; 
+    document.getElementById("icon-theme").src = "/icons/moon-stars-fill.svg";
+  } else {
+    document.body.style.backgroundColor = "#fff";
+    document.body.style.color = "#333";
+    document.getElementById("theme-toggle-btn").style.backgroundColor = "#fff"; 
+    document.getElementById("icon-theme").src="/icons/brightness-high-fill.svg";
+  }
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+  var savedTheme = getThemePreference();
+  if (savedTheme) {
+    applyTheme(savedTheme);
+  }
+});
+
 document.getElementById("dark-theme").addEventListener("click", function() {
-  document.body.style.backgroundColor = "#333";
-  document.body.style.color = "#fff";
-  document.getElementById("theme-toggle-btn").style.backgroundColor = "#333";
+  setThemePreference('dark');
+  applyTheme('dark');
 });
 
 document.getElementById("white-theme").addEventListener("click", function() {
-  document.body.style.backgroundColor = "#fff";
-  document.body.style.color = "#333";
-  document.getElementById("theme-toggle-btn").style.backgroundColor = "#fff";
+  setThemePreference('white');
+  applyTheme('white');
 });
+
